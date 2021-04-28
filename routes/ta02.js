fdce589890a04aee862f445ee685bb24690da98e
Dallas <deaton879@byui.edu>
2021-04-27 23:49:54 -0400

    Team Assign W02 complete (w/ stretch challenges)

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,12 +3,62 @@
 const express = require('express');
 const router = express.Router();
 
+// For core requirements
+const userArray = ['Dallas', 'Marcus', 'Steve', 'Alex'];
+// For core requirements
+
+// For stretch requirements
+let error = 0;
+// For stretch requirements
+
+router.post('/addUser', (req, res, next) => {
+    const newUser = req.body.newUser;
+
+    // For stretch requirement 2
+    const index = userArray.indexOf(newUser);
+    if(index !== -1) {
+        error = 2;
+
+    }else {
+        userArray.push(newUser);
+        error = 0;
+    }
+    // For stretch requirement 2
+
+    res.redirect('/ta02/');
+});
+
+router.post('/removeUser', (req, res, next) => {
+    const remUser = req.body.remUser;
+
+    const index = userArray.indexOf(remUser);
+    if(index !== -1) {
+        userArray.splice(index, 1);
+
+        // For stretch requirement 1
+        error = 0;
+    }
+    else {
+        error = 1;
+    }
+        // For stretch requirement 1
+
+    res.redirect('/ta02/');
+});
+
 router.get('/',(req, res, next) => {
-    res.render('pages', { 
+    res.render('pages/ta02', { 
         title: 'Team Activity 02', 
-        path: '/ta02', // For pug, EJS 
+        users: userArray,
+
+        // For stretch requirements
+        error: error,
+        // For stretch requirements
+
+        path: '/ta02/', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
+        
     });
 });
 
