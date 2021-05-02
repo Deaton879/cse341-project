0fd550bb061883faa080d505d065b3cf781b0a09
Dallas <deaton879@byui.edu>
2021-05-01 23:17:04 -0400

    finished for week 2

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')))
    .use('/ta04', ta04Routes)
    //.use('/prove01', prove01Routes)
    .use('/prove02', prove02Routes)
+   .use('/prove02/display', prove02Routes)
    .get('/', (req, res, next) => {
      // This is the primary index, always handled last. 
      res.render('pages/index', {title: 'Welcome to my CSE341 repo', path: '/'});
